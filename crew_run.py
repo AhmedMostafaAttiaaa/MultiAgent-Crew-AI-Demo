f14b6f5d15da9e63d8183706e4cec8796194a25b
Ahmed Mostafa <159034655+AhmedMostafaAttiaaa@users.noreply.github.com>
2025-04-13 00:06:16 +0200

    crew_run.py

--- crew_run.py
+++ crew_run.py
@@ -8,5 +8,34 @@ event_management_crew = Crew(
     verbose=True
 )
 
-result = event_management_crew.kickoff()
+""" event_details = {
+    'event_topic': "Tech Innovation Conference",
+    'event_description': "A gathering of tech innovators "
+                         "and industry leaders "
+                         "to explore future technologies.",
+    'event_city': "San Francisco",
+    'tentative_date': "2024-09-15",
+    'expected_participants': 500,
+    'budget': 20000,
+    'venue_type': "Conference Hall"
+} """
+
+result = event_management_crew.kickoff(inputs=event_details)
 print("Final Result:", result)
+
+
+""" 
+import json
+from pprint import pprint
+
+with open('venue_details.json') as f:
+   data = json.load(f)
+
+pprint(data)
+
+
+-------------------- 
+from IPython.display import Markdown
+Markdown("marketing_report.md")
+
+""""
